059c2ddacc8b16b4958cfaa93ac9f033fbbd5583
Andrew Thompson <andrew.thompson@mines.sdsmt.edu>
2013-11-23 11:13:26 -0700

    Initial Commit

---
+++ gen/edu/sdsmt/thompsonsamson/locationmapping/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.sdsmt.thompsonsamson.locationmapping;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
